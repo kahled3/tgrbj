9472a21c2e1a95ede1f2a844f506151c3efd95e9
kahled3 <126963482+kahled3@users.noreply.github.com>
2023-04-13 08:18:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client } = require('./lib/client')
+const { DATABASE, VERSION } = require('./config')
+const start = async () => {
+	try {
+		logger.info(`levanter ${VERSION}`)
+		await DATABASE.sync()
+		logger.info('Database syncing...')
+		const bot = new Client()
+		await bot.init()
+		await bot.connect()
+	} catch (error) {
+		logger.error(error)
+	}
+}
+start()
